d3e0c52a060e96705a57a7bc60e2c78eab7b8619
Admin <Admin@HSBC013-2024>
2025-09-13 15:23:04 +0530

    evenodd

--- teegala/src/evenodd/evenodd.java
+++ teegala/src/evenodd/evenodd.java
@@ -1,5 +1,16 @@
 package evenodd;
-
 public class evenodd {
+	    public static void main(String[] args)
+	    {
+	        int num = 10;
+	        if (num % 2 == 0) {
+	            System.out.println("Entered Number is Even");
+	        }
 
-}
+	        else {
+	            System.out.println("Entered Number is Odd");
+	        }
+	    }
+	}
+
+
