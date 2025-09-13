607fe72f1d142fdc8f60068d97fb84e45612b4ee
Admin <Admin@HSBC013-2024>
2025-09-13 11:40:49 +0530

    Initial commit

---
+++ teegala/src/teegala/demo.java
@@ -0,0 +1,28 @@
+package teegala;
+
+public class demo {
+
+	public static void main(String[] args) {
+		byte byteVar = 100;              
+        short shortVar = 32000;          
+        int intVar = 100000;             
+        long longVar = 10000000000L;     
+        float floatVar = 5.75f;        
+        double doubleVar = 19.99;        
+        char charVar = 'A';             
+        boolean boolVar = true;          
+        System.out.println("Byte Value: " + byteVar);
+        System.out.println("Short Value: " + shortVar);
+        System.out.println("Int Value: " + intVar);
+        System.out.println("Long Value: " + longVar);
+        System.out.println("Float Value: " + floatVar);
+        System.out.println("Double Value: " + doubleVar);
+        System.out.println("Char Value: " + charVar);
+        System.out.println("Boolean Value: " + boolVar);
+        String str = "Hello Java!";     
+        System.out.println("String Value: " + str);
+    }
+}
+
+
+				       
