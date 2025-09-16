9e5377ee3c53b9f386bcccb8d0632ea393cb4499
Admin <Admin@HSBC013-2024>
2025-09-16 15:09:55 +0530

    palindrome

--- teegala/src/com/tnsif/palindrome/palindrome.java
+++ teegala/src/com/tnsif/palindrome/palindrome.java
@@ -1,10 +1,31 @@
 package com.tnsif.palindrome;
+import java.util.Scanner;
 
 public class palindrome {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
 
-	}
+        System.out.print("Enter a number: ");
+        int num = sc.nextInt();
+
+        int original = num;
+        int reversed = 0;
+
+        while (num > 0) {
+            int digit = num % 10;            
+            reversed = (reversed * 10) + digit; 
+            num = num / 10;                   
+        }
+
+        if (original == reversed) {
+            System.out.println(original + " is a Palindrome Number.");
+        } else {
+            System.out.println(original + " is NOT a Palindrome Number.");
+        }
+}
 
 }
+
+	
+		        
