e3d16a5a3b9d63e7833f04c998b8da59d6682328
Admin <Admin@HSBC013-2024>
2025-09-17 22:47:22 +0530

    final

---
+++ teegala/src/com/tnsif/task5/Final.java
@@ -0,0 +1,17 @@
+package com.tnsif.task5;
+		final class Keys {   
+
+		    final int number = 10;   
+
+		    final void show() {      
+		        System.out.println("Number = " + number);
+		    }
+		}
+
+		public class Final{
+		    public static void main(String[] args) {
+		        Keys d = new Keys();
+		        d.show();
+		    }
+		}
+
