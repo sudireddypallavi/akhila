931be8d9803b429df794a3f3c4eabeb8a332a29f
Admin <Admin@HSBC013-2024>
2025-09-18 21:01:00 +0530

    phone

---
+++ teegala/src/com/tnsif/task6/PhoneMain.java
@@ -0,0 +1,13 @@
+package com.tnsif.task6;
+
+public class PhoneMain {
+
+	public static void main(String[] args) {
+		jio j=new jio();
+		j.call();
+		j.sms();
+		Samsung s=new Samsung();
+		s.call();
+		s.sms();
+	}
+}
--- teegala/src/com/tnsif/task6/Samsung.java
+++ teegala/src/com/tnsif/task6/Samsung.java
@@ -1,5 +1,12 @@
 package com.tnsif.task6;
 
-public class Samsung {
+public class Samsung implements Phone{
+	public void call() {
+		System.out.println("samsung call is accepted");
+	}
+	public void sms() {
+		System.out.println("samsung sms is accepted");
+	}
 
 }
+
