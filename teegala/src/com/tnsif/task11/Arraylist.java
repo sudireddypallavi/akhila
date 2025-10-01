7287c1f41722cdfd0a220a01c67b121d85cb67c9
Admin <Admin@HSBC013-2024>
2025-10-01 11:10:13 +0530

    Arraylist

---
+++ teegala/src/com/tnsif/task11/Arraylist.java
@@ -0,0 +1,16 @@
+package com.tnsif.task11;
+import java.util.*; 
+public class Arraylist  
+{ 
+public static void main(String aa[]) 
+{  
+ArrayList l=new ArrayList(); 
+l.add("10"); 
+l.add("20"); 
+System.out.println(l); 
+l.remove(0); 
+System.out.println(l); 
+System.out.println(l.get(0)); 
+} 
+} 
+
