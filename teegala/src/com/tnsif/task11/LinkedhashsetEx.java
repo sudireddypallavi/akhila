5b531a9d7900af38b85cc9b2302610a6c729bc85
Admin <Admin@HSBC013-2024>
2025-10-01 11:51:50 +0530

    linkedhashset

---
+++ teegala/src/com/tnsif/task11/LinkedhashsetEx.java
@@ -0,0 +1,19 @@
+package com.tnsif.task11;
+import java.util.*;
+public class LinkedhashsetEx {
+	    public static void main(String[] args) {
+	        LinkedHashSet set = new LinkedHashSet();
+	        set.add("10");
+	        set.add("20");
+	        set.add("30");
+	        set.add("40");
+	        set.add("50");
+	        System.out.println(set);
+	        System.out.println(set.contains("30"));
+	        set.remove("40");
+	        System.out.println(set);
+	        }
+	    }
+	
+
+
