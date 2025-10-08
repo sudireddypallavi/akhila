a48ee8c1f89a4ae78d0bfb53470489d9f7f61093
Admin <Admin@HSBC013-2024>
2025-10-08 19:04:27 +0530

    annotations

---
+++ teegala/src/com/tnsif/lambda/Annotations.java
@@ -0,0 +1,14 @@
+package com.tnsif.lambda;
+@FunctionalInterface
+interface Neww{
+	public void show();
+	
+}
+public class Annotations {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ teegala/src/com/tnsif/lambda/New.java
@@ -0,0 +1,7 @@
+package com.tnsif.lambda;
+@FunctionalInterface
+	public interface New {
+		public void show();
+	}
+
+
